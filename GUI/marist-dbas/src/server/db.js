851724df1c7b1dfa06462a04c9f95988ff43b471
ebolagranola <dale.doster1@marist.edu>
2022-11-02 19:56:39 -0400

    Updated application database config with database name

--- GUI/marist-dbas/src/server/db.js
+++ GUI/marist-dbas/src/server/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
   host: 'localhost',
   user:'root',
-  password:'root',
-  database:'elmasri_company'
+  password:'rootroot',
+  database:'myCms'
 });
 
 export default db;
