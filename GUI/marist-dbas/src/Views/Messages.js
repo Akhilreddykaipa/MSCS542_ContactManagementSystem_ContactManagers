0696ef32c632b57886214397a371694a8e857cd7
Dale Doster <dale.doster1@marist.edu>
2022-11-09 12:42:58 -0500

    Fixed bug with sidenav destroying message view

--- GUI/marist-dbas/src/Views/Messages.js
+++ GUI/marist-dbas/src/Views/Messages.js
@@ -5,46 +5,50 @@ const utils = require('../utils/utils.js');
 
 const Messages = (props) => {
   const [messages, setMessages] = useState([]);
+  const [firstRun, setFirstRun] = useState(true);
 
   useEffect(() => {
-    window.dbConnection.getMessages().then((result) => {
-      setMessages([...result]);
-    });
-    window.dbConnection.getEmployees().then((result) => {
-      let employeeData = result;
-      window.dbConnection.getEmployeeIDs({
-        email: $("#userProfile .userName").text()
-      }).then((result) => {
-        $(document).ready(() => {
-          $("#messagesBody tr").each((i, el) => {
-            let sender = $(el).find(".sender");
-            let receiver = $(el).find(".receiver");
-            if (sender.text() == result[0].ID) {
-              sender.text(result[0].Fname + " " + result[0].Lname);
-              employeeData.forEach((item, i) => {
-                for (let i = 0; i < Object.keys(item).length; i++) {
-                  let key = Object.keys(item)[i];
-                  if (item[key] === null) item[key] = "";
-                }
-                if (receiver.text() == item.ID) receiver.text(item.Fname + " " + item.Lname);
-              });
-            } else if (receiver.text() == result[0].ID) {
-              receiver.text(result[0].Fname + " " + result[0].Lname);
-              employeeData.forEach((item, i) => {
-                for (let i = 0; i < Object.keys(item).length; i++) {
-                  let key = Object.keys(item)[i];
-                  if (item[key] === null) item[key] = "";
-                }
-                if (sender.text() == item.ID) sender.text(item.Fname + " " + item.Lname);
-              });
-            } else {
-              $(el).remove();
-            }
-            $("#messagesBody").show();
+    if (firstRun) {
+      window.dbConnection.getMessages().then((result) => {
+        setMessages([...result]);
+      });
+      window.dbConnection.getEmployees().then((result) => {
+        let employeeData = result;
+        window.dbConnection.getEmployeeIDs({
+          email: $("#userProfile .userName").text()
+        }).then((result) => {
+          $(document).ready(() => {
+            $("#messagesBody tr").each((i, el) => {
+              let sender = $(el).find(".sender");
+              let receiver = $(el).find(".receiver");
+              if (sender.text() == result[0].ID) {
+                sender.text(result[0].Fname + " " + result[0].Lname);
+                employeeData.forEach((item, i) => {
+                  for (let i = 0; i < Object.keys(item).length; i++) {
+                    let key = Object.keys(item)[i];
+                    if (item[key] === null) item[key] = "";
+                  }
+                  if (receiver.text() == item.ID) receiver.text(item.Fname + " " + item.Lname);
+                });
+              } else if (receiver.text() == result[0].ID) {
+                receiver.text(result[0].Fname + " " + result[0].Lname);
+                employeeData.forEach((item, i) => {
+                  for (let i = 0; i < Object.keys(item).length; i++) {
+                    let key = Object.keys(item)[i];
+                    if (item[key] === null) item[key] = "";
+                  }
+                  if (sender.text() == item.ID) sender.text(item.Fname + " " + item.Lname);
+                });
+              } else {
+                $(el).remove();
+              }
+              $("#messagesBody").show();
+            });
           });
         });
       });
-    });
+      setFirstRun(false);
+    }
   }, []);
 
   return (
@@ -64,28 +68,21 @@ const Messages = (props) => {
           </thead>
           <tbody id="messagesBody">
             {messages.map((item, i) => {
-              return <MessageCell key={utils.newID() + Math.random()} messageData={item} />
+              return (
+                <>
+                  <tr>
+                    <td className="sender">{item.senderID}</td>
+                    <td className="receiver">{item.userID}</td>
+                    <td>{item.groupID}</td>
+                    <td>{item.Message}</td>
+                    <td>{utils.formatDate(item.Messagedate)}</td>
+                  </tr>
+                </>
+              )
             })}
           </tbody>
         </table>
       </div>
-    </>
-  );
-};
-
-const MessageCell = (props) => {
-  let dat = props.messageData;
-  let messageDate = utils.formatDate(dat.Messagedate);
-
-  return (
-    <>
-      <tr>
-        <td className="sender">{dat.senderID}</td>
-        <td className="receiver">{dat.userID}</td>
-        <td>{dat.groupID}</td>
-        <td>{dat.Message}</td>
-        <td>{messageDate}</td>
-      </tr>
     </>
   );
 }
