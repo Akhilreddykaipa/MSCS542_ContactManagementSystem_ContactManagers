268792ff7aec5a71d9bd19f6cc042584e0f0cfab
Dale Doster <dale.doster1@marist.edu>
2022-11-11 14:33:49 -0500

    Adding group view

--- GUI/marist-dbas/src/Views/Groups.js
+++ GUI/marist-dbas/src/Views/Groups.js
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react';
+import $ from 'jquery';
 import "../css/Groups.css";
+const utils = require('../utils/utils.js');
 
 const Groups = (props) => {
+  const [employees, setEmployees] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    window.dbConnection.getEmployeeIDs({
+      email: $("#userProfile .userName").text()
+    }).then((result) => {
+      setEmployees([...result]);
+      console.log(result);
+      setProfileData(result[0]);
+    });
+  }, []);
+
   return (
     <>
       <div id="contacts" className="container">
         <h2>Groups</h2>
         <hr/>
+        <div id="groupContainer">
+          <div className="group">
+
+          </div>
+          <div className="group">
+
+          </div>
+        </div>
       </div>
     </>
   );
