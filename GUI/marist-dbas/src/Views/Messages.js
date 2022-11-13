e88c3807ca5b60627e60f460abf79c11a152167e
Dale Doster <dale.doster1@marist.edu>
2022-11-13 11:50:05 -0500

    Added groupnames to messages view

--- GUI/marist-dbas/src/Views/Messages.js
+++ GUI/marist-dbas/src/Views/Messages.js
@@ -6,6 +6,7 @@ const utils = require('../utils/utils.js');
 const Messages = (props) => {
   const [messages, setMessages] = useState([]);
   const [firstRun, setFirstRun] = useState(true);
+  let groupDet;
 
   useEffect(() => {
     if (firstRun) {
@@ -14,34 +15,50 @@ const Messages = (props) => {
       });
       window.dbConnection.getEmployees().then((result) => {
         let employeeData = result;
-        window.dbConnection.getEmployeeIDs({
-          email: $("#userProfile .userName").text()
+
+        window.dbConnection.getGroupDetails().then((result) => {
+          console.log(result);
+          groupDet = result;
+
         }).then((result) => {
-          $(document).ready(() => {
-            $("#messagesBody tr").each((i, el) => {
-              let sender = $(el).find(".sender");
-              let receiver = $(el).find(".receiver");
-              if (sender.text() == result[0].ID) {
-                sender.text(result[0].Fname + " " + result[0].Lname);
-                employeeData.forEach((item, i) => {
-                  for (let i = 0; i < Object.keys(item).length; i++) {
-                    let key = Object.keys(item)[i];
-                    if (item[key] === null) item[key] = "";
+          window.dbConnection.getEmployeeIDs({
+            email: $("#userProfile .userName").text()
+          }).then((result) => {
+            $(document).ready(() => {
+              $("#messagesBody tr").each((i, el) => {
+                let sender = $(el).find(".sender");
+                let receiver = $(el).find(".receiver");
+                let groupName = $(el).find(".groupName");
+                if (sender.text() == result[0].ID) {
+                  sender.text(result[0].Fname + " " + result[0].Lname);
+                  employeeData.forEach((item, i) => {
+                    for (let i = 0; i < Object.keys(item).length; i++) {
+                      let key = Object.keys(item)[i];
+                      if (item[key] === null) item[key] = "";
+                    }
+                    if (receiver.text() == item.ID) receiver.text(item.Fname + " " + item.Lname);
+                  });
+                } else if (receiver.text() == result[0].ID) {
+                  receiver.text(result[0].Fname + " " + result[0].Lname);
+                  employeeData.forEach((item, i) => {
+                    for (let i = 0; i < Object.keys(item).length; i++) {
+                      let key = Object.keys(item)[i];
+                      if (item[key] === null) item[key] = "";
+                    }
+                    if (sender.text() == item.ID) sender.text(item.Fname + " " + item.Lname);
+                  });
+                } else {
+                  $(el).remove();
+                }
+                groupDet.forEach((item, i) => {
+                  console.log(item);
+                  if (groupName.text() == item.ID) {
+                    groupName.text(item.GroupName);
                   }
-                  if (receiver.text() == item.ID) receiver.text(item.Fname + " " + item.Lname);
                 });
-              } else if (receiver.text() == result[0].ID) {
-                receiver.text(result[0].Fname + " " + result[0].Lname);
-                employeeData.forEach((item, i) => {
-                  for (let i = 0; i < Object.keys(item).length; i++) {
-                    let key = Object.keys(item)[i];
-                    if (item[key] === null) item[key] = "";
-                  }
-                  if (sender.text() == item.ID) sender.text(item.Fname + " " + item.Lname);
-                });
-              } else {
-                $(el).remove();
-              }
+
+                // groupName.text()
+              });
               $("#messagesBody").show();
             });
           });
@@ -73,7 +90,7 @@ const Messages = (props) => {
                   <tr>
                     <td className="sender">{item.senderID}</td>
                     <td className="receiver">{item.userID}</td>
-                    <td>{item.groupID}</td>
+                    <td className="groupName">{item.groupID}</td>
                     <td>{item.Message}</td>
                     <td>{utils.formatDate(item.Messagedate)}</td>
                   </tr>
